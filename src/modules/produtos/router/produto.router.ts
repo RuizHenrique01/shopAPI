7f3c8a163e889b2c7e81463bf7e23fa532f0a3cf
RuizHenrique01 <affonsohenriqueruiz@gmail.com>
2021-11-26 14:04:15 -0400

    <add> adicionando validação de rotas

--- src/modules/produtos/router/produto.router.ts
+++ src/modules/produtos/router/produto.router.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import ProdutoController from "../controllers/ProdutoController";
+import { celebrate, Joi, Segments } from "celebrate";
 
 const produtoRouter = Router();
 const produtoController = new ProdutoController();
 
 produtoRouter.get("/", produtoController.index);
-produtoRouter.post("/", produtoController.create);
+produtoRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      nome: Joi.string().required(),
+      descricao: Joi.string(),
+      preco: Joi.number().precision(2).required(),
+    },
+  }),
+  produtoController.create
+);
 
 export default produtoRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,10 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
-import AppError from "../errors/AppError";
 import cors from "cors";
+import { errors } from "celebrate";
 import routes from "./routes";
+import AppError from "../errors/AppError";
 import "@shared/typeorm";
 
 const app = express();
@@ -11,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
